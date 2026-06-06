7e2fa6a9c95e2f81183464556489751ad536720a
Swati Basliyal <swatibasliyal24@gmail.com>
2026-06-06 08:14:20 +0530

    added new array

--- main.c
+++ main.c
@@ -1,8 +1,61 @@
 #include <stdio.h>
-
+/*
 int main()
 {
     printf("Hello World");
     return 0;
 }
-//dsa test
+*/
+int main(){
+    /*
+    int a,b,c,d,e;
+    printf("Enter 5 numbers: ");
+    scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+     //to print the largest number among the 5 numbers
+    int largest = a;
+    if(b > largest)
+        largest = b;
+    if(c > largest)
+        largest = c;
+    if(d > largest)
+        largest = d;
+    if(e > largest)
+        largest = e;
+    printf("The largest number is: %d", largest);
+    */
+   /*
+   int i=1,n,sum=0;
+   printf("Enter a number: ");
+   scanf("%d", &n);
+   for (i=1;i<=n;i++){
+        printf("%d ",i);
+   }*/
+  /*
+  for (i;i<=n;i++){
+        sum = sum + i;
+}printf("The sum of the first %d numbers is: %d", n, sum);
+*/
+/*
+int i=1,largest=0;
+for (i;i<=10;i++){
+    if(i>largest)
+        largest = i;
+   }
+   printf("The largest number is: %d", largest);
+   */
+  // to count how many times a number appears in an array
+  /*
+int arr[10],i,n,count=0;
+printf("Enter 10 numbers: ");
+for(i=0;i<10;i++){
+    scanf("%d", &arr[i]);
+}
+printf("Enter a number to count its occurrences: ");
+scanf("%d", &n);
+for(i=0;i<10;i++){
+    if(arr[i]==n)
+        count++;
+}
+printf("The number %d appears %d times in the array.", n, count);*/
+   return 0;
+}
